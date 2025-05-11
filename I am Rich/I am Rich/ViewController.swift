0537408a234066710ca358330e0a9e4689a4f55c
Danny <46591199+D-nov@users.noreply.github.com>
2025-05-11 14:59:55 +0300

    initial commit

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Danny on 11.05.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
