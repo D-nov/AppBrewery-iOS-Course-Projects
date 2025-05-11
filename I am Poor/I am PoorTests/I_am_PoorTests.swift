cea04d944fa54cf05759d3c090b166053ab2d17a
Danny <46591199+D-nov@users.noreply.github.com>
2025-05-11 21:49:38 +0300

    I am Poor challenge

---
+++ I am Poor/I am PoorTests/I_am_PoorTests.swift
@@ -0,0 +1,17 @@
+//
+//  I_am_PoorTests.swift
+//  I am PoorTests
+//
+//  Created by Danny on 11.05.25.
+//
+
+import Testing
+@testable import I_am_Poor
+
+struct I_am_PoorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
